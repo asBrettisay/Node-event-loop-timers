17daa70eec4cfb2a83eeaa2216754d9504e22221
Brett Caudill <brett.caudillo@gmail.com>
2017-03-30 09:50:21 -0600

    Initial commit.

---
+++ index.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+/**
+ * Each of these is executed in the main module.
+ * Non-deterministic order of events
+ */
+setImmediate(() => {
+  console.log('Main immediate');
+})
+
+setTimeout(() => {
+  console.log('Main timeout');
+}, 0)
+
+process.nextTick(() => {
+  console.log('main tick')
+})
+
+/**
+ * Executed in the event loop, because these happen after I/O.
+ */
+
+fs.readFile('./test.txt', () => {
+  // Check phase
+  setImmediate(() => {
+    console.log('Immediate')
+  })
+
+  // Timer phase (Ordered by poll phase)
+  setTimeout(() => {
+    console.log('Timeout')
+  }, 0)
+  
+  // Immediately after poll phase
+  process.nextTick(() => {
+    console.log('Tick')
+  })
+})
